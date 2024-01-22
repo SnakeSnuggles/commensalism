5361965e83f8d69d63d5ebe419badf62b12bb829
SnakeSnuggles <jacobsylvester6@gmail.com>
2024-01-21 17:57:01 -0700

    first commit

---
+++ src/main/java/net/snakesnuggles/commensalism/Commensalism.java
@@ -0,0 +1,16 @@
+package net.snakesnuggles.commensalism;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Commensalism implements ModInitializer {
+	public static final String MOD_ID = "commensalism";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/snakesnuggles/commensalism/CommensalismClient.java
@@ -0,0 +1,10 @@
+package net.snakesnuggles.commensalism;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CommensalismClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
